40ed7efa33b0d5cfd5e1c4f7c334293682a5f7a9
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-02-23 13:41:25 -0800

    Add Deck component into boilerplate

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Deck from './components/Deck';
+
 class App extends Component {
   render() {
     return (
@@ -20,6 +22,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Deck/>
       </div>
     );
   }
