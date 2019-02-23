24905046dc18438aeb6335aa0ba8b7cc42414b24
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-02-23 13:40:07 -0800

    Add some initial styles for a card

---
+++ src/css/card-style.js
@@ -0,0 +1,22 @@
+const card = {
+  border: "2px solid black",
+  height: "400px",
+  width: "300px",
+  borderRadius: "25px",
+  margin: "50px"
+}
+
+const header = {
+  
+}
+
+const body = {
+  
+}
+
+
+export const cardStyle = {
+  card,
+  header,
+  body
+}
