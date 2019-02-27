452d79ace3a14af94ac1ec0bcd5ca05e84cbc829
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-02-27 15:56:42 -0800

    Edit styles for cards

--- src/css/card-style.js
+++ src/css/card-style.js
@@ -3,15 +3,24 @@ const card = {
   height: "400px",
   width: "300px",
   borderRadius: "25px",
-  margin: "50px"
+  margin: "50px",
 }
 
 const header = {
-  
+  backgroundColor: "hotpink",
+  height: "100px",
+  width: "300px",
+  borderRadius: "22px 22px 0px 0px", /* top left, top right, bot right, bot left */
 }
 
 const body = {
-  
+  backgroundColor: "black",
+  height: "300px",
+  width: "300px",
+  borderRadius: "0px 0px 22px 22px",
+  color: "white",
+  fontSize: "36px",
+  fontStyle: "bold"
 }
 
 
