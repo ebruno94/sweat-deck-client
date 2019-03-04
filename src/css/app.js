b04ec6567c39dd1d6e28c1c95408232bc9338deb
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-03-04 14:19:12 -0800

    Change into absolute sizing instead of relative. Also adds footer style

---
+++ src/css/app.js
@@ -0,0 +1,29 @@
+const container = {
+  width: "500px",
+  height: "600px",
+  display: "block",
+  marginLeft: "auto",
+  marginRight: "auto",
+  marginTop: "40px",
+  marginBottom: "40px",
+  padding: "0px",
+  border: "4px solid black",
+  borderRadius: "20px 20px 20px 20px",
+  boxShadow: "0px 0px 25px black",
+  backgroundColor: "#1d1d1e"
+}
+
+const footer = {
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  width: "100%",
+  backgroundColor: "black",
+  color: "white",
+  textAlign: "center"
+}
+
+export const AppStyle = {
+  container,
+  footer
+}
