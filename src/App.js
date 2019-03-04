3cfb43d07aef8e5776f01597ce69a2afb719d51f
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-03-04 14:13:28 -0800

    Add react-router-dom dependency and apply Routes and Links to navigate to different pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
+import {AppStyle} from './css/app';
 import Deck from './components/Deck';
+import Welcome from './components/Welcome';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Deck/>
+        <Router>
+          <div style={AppStyle.container}>
+            <Route exact path='/' component={Welcome}></Route>
+            <Route exact path='/deck' component={Deck}></Route>
+          </div>
+        </Router>
+        <div style={AppStyle.footer}>
+          Copyright © 2019 www.mysweatdeck.com  All Rights Reserved
+        </div>
       </div>
     );
   }
