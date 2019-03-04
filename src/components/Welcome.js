969bdf7905cb91596b3a6045a2b89d05d25eb569
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-03-04 14:18:29 -0800

    Create welcome page for when App first loads

---
+++ src/components/Welcome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import {ButtonStyle} from './../css/button.js';
+import Button from './Button';
+
+const mainLogo = require('./../assets/white_logo_transparent.png');
+
+const Welcome = () => {
+  return (
+    <div >
+      <img src={mainLogo} alt='Sweat Deck Logo' style={{display: 'block', margin: 'auto', marginTop: '50px', marginBottom: '60px', height: '330px', width: '330px'}}/>
+      <Button linkTo='deck' buttonName='LAUNCH'/>
+    </div>
+  )
+}
+
+export default Welcome;
