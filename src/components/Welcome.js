e439897d2cd0ee21843c87e3e6485cb81cb23223
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-03-05 21:44:42 -0800

    Add SignIn component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import Button from './Button';
+import SignIn from './SignIn';
 
 const mainLogo = require('./../assets/white_logo_transparent.png');
 
 const Welcome = () => {
   return (
     <div>
-      <img src={mainLogo} alt='Sweat Deck Logo' style={{display: 'block', margin: 'auto', marginTop: '50px', marginBottom: '60px', height: '330px', width: '330px'}}/>
+      <img src={mainLogo} alt='Sweat Deck Logo' style={{display: 'block', margin: 'auto', marginTop: '20px', marginBottom: '15px', height: '330px', width: '330px'}}/>
+      <SignIn/>
       <Button linkTo='deck' buttonName='LAUNCH'/>
     </div>
   )
