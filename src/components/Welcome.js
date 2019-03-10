1f4a753ac91aa378544ed7e539b7d27fb5eac51a
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-03-10 11:26:03 -0700

    Add register link down under Launch button

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from './Button';
+import FancyLink from './FancyLink';
 import SignIn from './SignIn';
 
 const mainLogo = require('./../assets/white_logo_transparent.png');
@@ -10,7 +11,8 @@ const Welcome = () => {
     <div>
       <img src={mainLogo} alt='Sweat Deck Logo' style={{display: 'block', margin: 'auto', marginTop: '20px', marginBottom: '15px', height: '330px', width: '330px'}}/>
       <SignIn/>
-      <Button linkTo='deck' buttonName='LAUNCH'/>
+      <Button linkTo='/deck' buttonName='LAUNCH'/>
+      <FancyLink linkTo='/register' linkName='REGISTER'/>
     </div>
   )
 }
