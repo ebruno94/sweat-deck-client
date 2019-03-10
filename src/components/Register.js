802502f1526c258356ae49411f9f2209e80d5959
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-03-10 11:27:33 -0700

    Create form for registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {AppStyle} from './../css/app.js';
 import Button from './Button';
+import FancyLink from './FancyLink';
 
 class Register extends Component {
   constructor(props){
@@ -49,42 +50,67 @@ class Register extends Component {
 
   render(){
     return(
-      <div style={AppStyle.form}>
+      <div style={AppStyle.regForm}>
         <form onSubmit={this.handleSubmission}>
+          <h2 style={{marginBottom: '0px'}}> PLEASE FILL OUT THE FORM BELOW </h2>
+          <hr style={{...AppStyle.line, marginBottom: '10px'}}/>
+          <label>First Name</label>
           <div>
             <input
+              style={AppStyle.input}
               type='text'
               id='firstName'
               placeholder='John / Jane'
               onChange={this.handleFirstName} />
           </div>
+          <br/>
+          <label>Last Name</label>
           <div>
             <input
+              style={AppStyle.input}
               type='text'
               id='lastName'
               placeholder='Doe'
               onChange={this.handleLastName} />
           </div>
-            <div>
+          <br/>
+          <label>E-MAIL ADDRESS</label>
+          <div>
             <input
+              style={AppStyle.input}
               type='text'
               id='email'
               placeholder='example1234@email.com'
               onChange={this.handleEmail}/>
           </div>
+          <br/>
+          <label>PASSWORD</label>
           <div>
             <input
+              style={AppStyle.input}
               type='password'
               id='password'
               placeholder='Must be 6 - 12 characters'
               onChange={this.handlePassword} />
           </div>
+          <br/>
+          <label>CONFIRM PASSWORD</label>
           <div>
             <input
+              style={AppStyle.input}
               type='password'
               id='confirmation'
               placeholder='Confirm Password'
               onChange={this.handlePasswordConfirmation} />
+          </div>
+          <hr style={{...AppStyle.line, marginBottom: '0px', marginTop: '15px'}}/>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <div style={{margin: 'auto'}}>
+              <FancyLink linkTo='/' linkName='RETURN'/>
+            </div>
+            <div style={{margin: 'auto'}}>
+              <Button buttonName='REGISTER' type='submit'/>
+            </div>
           </div>
         </form>
       </div>
