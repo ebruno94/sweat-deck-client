86cb83c4f25c6bc2328da4761f72eda09e72dee1
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-03-10 20:20:26 -0700

    Add logic to determine user's age given user's birthday

--- src/components/Register.js
+++ src/components/Register.js
@@ -9,13 +9,15 @@ class Register extends Component {
     this.state = {
       firstName: '',
       lastName: '',
-      age: '',
+      age: 0,
       password: '',
       validEmail: false,
       validPassword: false,
+      registrationSuccess: false
     }
     this.handleFirstName = this.handleFirstName.bind(this);
     this.handleLastName = this.handleLastName.bind(this);
+    this.handleBirthday = this.handleBirthday.bind(this);
     this.handleEmail = this.handleEmail.bind(this);
     this.handlePassword = this.handlePassword.bind(this);
     this.handlePasswordConfirmation = this.handlePasswordConfirmation.bind(this);
@@ -24,6 +26,7 @@ class Register extends Component {
 
   handleSubmission(e) {
     e.preventDefault();
+    const {firstName, lastName, age, password, validEmail } = this.state;
   };
 
   handleFirstName(e){
@@ -32,6 +35,15 @@ class Register extends Component {
 
   handleLastName(e){
     this.setState({lastName: e.target.value});
+  };
+
+  handleBirthday(e){
+    let today = new Date();
+    let birthDate = new Date(e.target.value);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) age--;
+    this.setState({age});
   };
 
   handleEmail(e){
@@ -49,54 +61,68 @@ class Register extends Component {
   };
 
   render(){
+    const divBreaker = {
+      marginBottom: '11px',
+    }
+
     return(
       <div style={AppStyle.regForm}>
         <form onSubmit={this.handleSubmission}>
           <h2 style={{marginBottom: '0px'}}> PLEASE FILL OUT THE FORM BELOW </h2>
           <hr style={{...AppStyle.line, marginBottom: '10px'}}/>
           <label>First Name</label>
-          <div>
+          <div style={divBreaker}>
             <input
+              required
               style={AppStyle.input}
               type='text'
               id='firstName'
               placeholder='John / Jane'
               onChange={this.handleFirstName} />
           </div>
-          <br/>
           <label>Last Name</label>
-          <div>
+          <div style={divBreaker}>
             <input
+              required
               style={AppStyle.input}
               type='text'
               id='lastName'
               placeholder='Doe'
               onChange={this.handleLastName} />
           </div>
-          <br/>
-          <label>E-MAIL ADDRESS</label>
-          <div>
+          <label>DATE OF BIRTH</label>
+          <div style={divBreaker}>
             <input
+              required
+              style={AppStyle.input}
+              type='date'
+              id='birthday'
+              onChange={this.handleBirthday}/>
+          </div>
+          <label>E-MAIL ADDRESS</label>
+          <div style={divBreaker}>
+            <input
+              required
               style={AppStyle.input}
               type='text'
               id='email'
               placeholder='example1234@email.com'
               onChange={this.handleEmail}/>
           </div>
-          <br/>
           <label>PASSWORD</label>
-          <div>
+          <div style={divBreaker}>
             <input
+              required
               style={AppStyle.input}
               type='password'
               id='password'
               placeholder='Must be 6 - 12 characters'
               onChange={this.handlePassword} />
           </div>
-          <br/>
           <label>CONFIRM PASSWORD</label>
-          <div>
+          <div style={divBreaker}>
             <input
+              required
               style={AppStyle.input}
               type='password'
               id='confirmation'
@@ -106,7 +132,7 @@ class Register extends Component {
           <hr style={{...AppStyle.line, marginBottom: '0px', marginTop: '15px'}}/>
           <div style={{display: 'flex', justifyContent: 'center'}}>
             <div style={{margin: 'auto'}}>
-              <FancyLink linkTo='/' linkName='RETURN'/>
+              <FancyLink linkTo='/' linkName='NEVERMIND, RETURN'/>
             </div>
             <div style={{margin: 'auto'}}>
               <Button buttonName='REGISTER' type='submit'/>
