295499c69b418c07f277d4ba6d2893784ca9ba55
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-03-12 16:36:05 -0700

    Remove signIn button from welcome component and add into sign in component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-import Button from './Button';
 import FancyLink from './FancyLink';
 import SignIn from './SignIn';
 
 const mainLogo = require('./../assets/white_logo_transparent.png');
 
-const Welcome = () => {
+const Welcome = (props) => {
   return (
     <div>
       <img src={mainLogo} alt='Sweat Deck Logo' style={{display: 'block', margin: 'auto', marginTop: '20px', marginBottom: '15px', height: '330px', width: '330px'}}/>
-      <SignIn/>
-      <Button linkTo='/deck' buttonName='LAUNCH'/>
-      <FancyLink linkTo='/register' linkName='REGISTER'/>
+      <SignIn {...props} onSignIn={props.onSignIn}/>
+      <div style={{marginTop: '85px'}}>
+        <FancyLink linkTo='/register' linkName='REGISTER'/>
+      </div>
     </div>
   )
 }
