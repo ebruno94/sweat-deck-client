7d6cf8c4ccd3bf2294f3e57f8a0874460a1d6943
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-03-12 16:35:22 -0700

    Complete sign in component and redirect user to /deck if signin successful

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,20 +1,78 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 import {AppStyle} from './../css/app.js';
+import Button from './Button';
 
 class SignIn extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      emailFound: true,
+    }
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+    this.handleSubmission = this.handleSubmission.bind(this);
+  }
+
+  handleEmail(e){
+    this.setState({email: e.target.value})
+  }
+
+  handlePassword(e){
+    this.setState({password: e.target.value})
+  }
+
+  handleSubmission(e){
+    e.preventDefault();
+    axios.get(`/users/email/${this.state.email.toLowerCase()}`).then(res => {
+      let user = res.data;
+      this.setState({emailFound: true});
+      if (res.data.password === this.state.password){
+        this.props.onSignIn(user)
+        this.setState({email: '', password: ''})
+        this.props.history.push('/deck');
+      } else if (res.status === 404){
+        this.setState({email: '', password: '', emailFound: false});
+      }
+    }).catch((e) => {
+      this.setState({email: '', password: '', emailFound: false});
+    })
+  }
+
   render() {
+    let errorMessage = 'INVALID EMAIL / PASSWORD';
     return(
-      <div style={AppStyle.form}>
-        <form>
-          <div>
-            <input style={AppStyle.input} type='text' name='email' placeholder='Enter E-mail address'/>
-          </div>
-          <hr style={AppStyle.line}/>
-          <div>
-            <input style={AppStyle.input} type='password' name='password' placeholder='enter password'/>
-          </div>
-        </form>
+      <div>
+        <div style={{display: 'flex', justifyContent: 'center', color: 'red'}}>
+          {(!this.state.emailFound) ? errorMessage : ''}
+        </div>
+        <div style={(!this.state.emailFound) ? AppStyle.form : {...AppStyle.form, marginTop: '14px'}}>
+          <form onSubmit={this.handleSubmission}>
+            <div>
+              <input
+                required
+                style={(this.state.emailFound) ? AppStyle.input : AppStyle.inputError}
+                type='text'
+                name='email'
+                placeholder='Enter E-mail address'
+                onChange={this.handleEmail}/>
+            </div>
+            <hr style={AppStyle.line}/>
+            <div style={{marginBottom: '5px'}}>
+              <input
+                style={AppStyle.input}
+                type='password'
+                name='password'
+                placeholder='enter password'
+                onChange={this.handlePassword}/>
+            </div>
+
+            <Button buttonName='LAUNCH' type='submit'/>
+          </form>
+        </div>
       </div>
     )
   };
