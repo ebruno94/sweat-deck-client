6d1f6f0c131085cd62c638e2eb80f879b1bb1500
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-03-13 18:27:05 -0700

    Add /build

---
+++ build/precache-manifest.c37ce20da00844196b5b93af3d27c9dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "96d5a089062ab3e34c47cfdc17d9d1d6",
+    "url": "/static/media/white_logo_transparent.96d5a089.png"
+  },
+  {
+    "revision": "0e0f3f2f2cb7fd99a9afd332983d1e0a",
+    "url": "/static/media/BebasNeue-Book.0e0f3f2f.ttf"
+  },
+  {
+    "revision": "50c63ebc60e4f6f502dd09b59c55146b",
+    "url": "/static/media/BebasNeue-Bold.50c63ebc.ttf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "80223f10f8d492f4dcde",
+    "url": "/static/js/main.80223f10.chunk.js"
+  },
+  {
+    "revision": "cfb221d3046cc880ba15",
+    "url": "/static/js/2.cfb221d3.chunk.js"
+  },
+  {
+    "revision": "80223f10f8d492f4dcde",
+    "url": "/static/css/main.4cf2304e.chunk.css"
+  },
+  {
+    "revision": "09ebece873624a46afa565d90828550f",
+    "url": "/index.html"
+  }
+];
